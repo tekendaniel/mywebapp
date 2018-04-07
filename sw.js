82ffbad547a7e01fcb036a3e3ea8c56057383bdb
tekendaniel <34918350+tekendaniel@users.noreply.github.com>
2018-04-06 21:00:32 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,2 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
-
- 
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
