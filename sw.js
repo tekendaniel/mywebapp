e119011d812571b7fe11c325b82c7109365a9685
Daniel Arellano Cornejo <tekendaniel@gmail.com>
2018-04-07 15:00:57 -0500

    Update index.html sw.js

--- sw.js
+++ sw.js
@@ -51,6 +51,15 @@ plugins: [
 );
 
 
+workbox.precaching.precacheAndRoute([
+	'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm',
+    'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl',
+    { url: '/index.html', revision: '383676' },
+]);
+
+
+
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
