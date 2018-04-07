9560f862a52f5b3789466788ac7fa9c9545816ea
Daniel Arellano Cornejo <tekendaniel@gmail.com>
2018-04-07 16:35:33 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -60,24 +60,7 @@ workbox.precaching.precacheAndRoute([
 
 
 
-function askPermission() {
-  return new Promise(function(resolve, reject) {
-    const permissionResult = Notification.requestPermission(function(result) {
-      resolve(result);
-    });
 
-    if (permissionResult) {
-      permissionResult.then(resolve, reject);
-    }
-  })
-  .then(function(permissionResult) {
-    if (permissionResult !== 'granted') {
-      throw new Error('We weren\'t granted permission.');
-    }
-  });
-}
-
-askPermission();
 
 
 
