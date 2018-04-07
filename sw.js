5a2145a530c013448a18738e72096c09f982622b
tekendaniel <34918350+tekendaniel@users.noreply.github.com>
2018-04-06 21:16:18 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,11 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+  
+  workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.networkFirst()
+);
 
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
