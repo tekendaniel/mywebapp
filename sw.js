1e76c31fb3ecdec8da95a750ee64b08d6ecdaf2d
Daniel Arellano Cornejo <tekendaniel@gmail.com>
2018-04-08 10:53:04 -0500

    Create css\bpp.css

--- sw.js
+++ sw.js
@@ -50,9 +50,6 @@ plugins: [
 })
 );
 
-
-
-
 workbox.routing.registerRoute(
   'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm',
   workbox.strategies.networkFirst(),
